19617e9bd7309f4f7cc4d0f7dfe325a83cff3269
frazer.sado <sadofrazer@gmail.com>
2021-10-22 19:50:12 +0200

    add source

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -6,6 +6,7 @@ mkdir ~/.bash_profile
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64/" >> ~/.bash_profile
 echo "export JRE_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64/jre" >> ~/.bash_profile
 source ~/.bash_profile
+source /root/.bash_profile
 echo $JAVA_HOME
 yum install wget -y
 yum -y install epel-release
