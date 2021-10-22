ed8a1d706c74edb1b25baf8901119cc4317b0da3
frazer.sado <sadofrazer@gmail.com>
2021-10-22 18:21:22 +0200

    edit dockerfile

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -10,7 +10,7 @@ yum install wget -y
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum -y install jenkins
-#systemctl start jenkins
-#systemctl status jenkins
+systemctl start jenkins
+systemctl status jenkins
 yum -y install epel-release
 yum -y install nginx
